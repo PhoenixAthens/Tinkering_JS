63e607e8c57536a6ecc8d384b46190a2e7a5b725
Anmol Khanna <102641709+PhoenixAthens@users.noreply.github.com>
2024-04-01 23:19:25 +0530

    Delete digging_into_fs_module directory

--- digging_into_fs_module/checkingPermissionsOnAFile.js
+++
@@ -1,7 +0,0 @@
-import { access, constants } from "node:fs";
-const file = "/Users/anmolkhanna/Downloads/guessGame";
-access(file, constants.F_OK, (err) => {
-  console.log(`${file} ${err ? "does not exist" : "exists"}`);
-});
-//output /Users/anmolkhanna/Downloads/guessGame exists
-//In notes for this udemy course, mention what happended a few minutes ago
