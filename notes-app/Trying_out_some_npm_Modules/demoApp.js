e56ad062b00171c19838cd9a184061c3c0093263
Phoenix <anmolkhanna2003@gmail.com>
2024-04-04 18:53:26 +0530

    looking into some npm modules

--- notes-app/Trying_out_some_npm_Modules/demoApp.js
+++
@@ -1,3 +0,0 @@
-import validator from "validator";
-console.log(validator.isURL("http://localhost:8000"));
-console.log(validator.isEmail("anmolkhanna2003@gmail.com"));
---
+++ notes-app/Trying_out_some_npm_Modules/tryingOutChalk.js
@@ -0,0 +1,3 @@
+import chalk from "chalk";
+console.log(chalk.green("SUCCESS!"));
+console.log(chalk.bold.underline.red("RUN FOOL!"));
