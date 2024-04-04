d7c3277dd30bf3109d11aba3845d46982c9c0311
Phoenix <anmolkhanna2003@gmail.com>
2024-04-04 18:54:01 +0530

    getting deeper into the project

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,2 +1,19 @@
 const fromNotes = require("./notes.js");
 console.log(fromNotes());
+
+//accessing the command line arguments
+console.log(process.argv);
+//Simply running node app.js, gives us
+// [
+//   '/Users/anmolkhanna/.nvm/versions/node/v21.6.1/bin/node',
+//   '/Users/anmolkhanna/Downloads/Programming_Savings_2024/Node_with_Andrew/Notes_App/notes-app/app.js'
+// ]
+
+//node app.js andrew --title="hello" --content="I lived in the hinterland"
+// [
+//   '/Users/anmolkhanna/.nvm/versions/node/v21.6.1/bin/node',
+//   '/Users/anmolkhanna/Downloads/Programming_Savings_2024/Node_with_Andrew/Notes_App/notes-app/app.js',
+//   'andrew',
+//   '--title=hello',
+//   '--content=I lived in the hinterland'
+// ]
