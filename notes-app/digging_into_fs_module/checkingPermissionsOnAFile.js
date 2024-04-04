d4363dd137bbf2856d7ca3d72319a5aab3681830
Phoenix <anmolkhanna2003@gmail.com>
2024-04-04 18:52:38 +0530

    learning cmdline args and more about the fs module

--- notes-app/digging_into_fs_module/checkingPermissionsOnAFile.js
+++ notes-app/digging_into_fs_module/checkingPermissionsOnAFile.js
@@ -1,4 +1,6 @@
-import { access, constants } from "node:fs";
+import { access, constants, open, close, write } from "node:fs";
+import { Buffer } from "node:buffer";
+
 const file = "/Users/anmolkhanna/Downloads/guessGame";
 const split = file.split("/");
 access(file, constants.F_OK, (err) => {
@@ -45,3 +47,46 @@ access(file, constants.R_OK | constants.W_OK | constants.X_OK, (err) => {
 //OUTPUT = guessGame, is not RWX
 //after running `chmod ugo+x guessGame`
 //OUTPUT = guessGame, is RWX
+
+let path = "/Users/anmolkhanna/Downloads/Scrap_Use/discarded.txt";
+
+//in the HOF below, we first check if the file alreday exists, if it does we return
+//else we open the file using opne, which automatically creates the file and use the file-descriptor to write data to the file
+//and finally close the file at the end.
+function writeData(fd) {
+  let data = Buffer.from("Hello World! You still persisting with your cruely?");
+  write(fd, data, 0, data.length, null, (err) => {
+    if (err) {
+      console.log("Failed to write to file!");
+    } else {
+      console.log("Data written successfully!");
+    }
+  });
+}
+access(path, (err) => {
+  if (!err) {
+    console.log("File already exists");
+    return;
+  }
+  open(path, "wx", (err, fd) => {
+    if (err) {
+      console.log("error opening file!");
+      throw err;
+    }
+    try {
+      writeData(fd);
+    } finally {
+      close(fd, (err) => {
+        //the callback in close is triggered regardless of whether an error occurs or not!
+        if (err) throw err;
+      });
+    }
+  });
+});
+//if the file alrady exists
+//OUTPUT- File already exists
+
+//Let's change discard.c to discarded.text, a file that doesn't exist
+//OUTPUT:Data written successfully!
+//`cat discarded.txt`
+//Hello World! You still persisting with your cruely?%
